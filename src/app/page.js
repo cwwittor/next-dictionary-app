7105a627d7380f7c7381008a7ea0ebb1bd7f4a49
Christian Wittorp <christian@swarminteractive.com>
2023-11-06 10:37:50 -0500

    fix: synonyms added, search working correctly, minor style changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,69 @@
+"use client";
 import Image from "next/image";
+// import { useEffect, useState } from "react";
+
+// import { useClient } from "react";
 import { darkMode } from "./index.js";
 import Head from "next/head";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./fontawesome";
 
+import { useEffect, useState } from "react";
+
+// import WordDefinition from "./worddefinition.js";
+import DictionaryHelp from "./dictionaryHelp.js";
+
 export default function Home() {
+  // const [selectedFont, setSelectedFont] = useState("inter");
+
+  // const handleFontChange = (event) => {
+  //   setSelectedFont(event.target.value);
+  // };
+  const [searchValue, setSearchValue] = useState("");
+
+  const [elementsString, setElementsString] = useState("");
+
+  function handlePlayAudio() {
+    const audioPlayer = document.getElementById("audioPlayer");
+    audioPlayer.play();
+  }
+
+  const fetchData = async () => {
+    try {
+      // Call the `DictionaryHelp` function with the updated search value
+      const updatedElementsString = await DictionaryHelp(
+        searchValue,
+        handlePlayAudio // Pass handlePlayAudio as a prop
+      );
+
+      // Update the state with the elements string
+      setElementsString(updatedElementsString);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault(); // Prevent form submission
+
+    // Perform search operation
+    fetchData();
+  };
+
+  // const playButton = document.getElementById("playButton");
+  // const audioPlayer = document.getElementById("audioPlayer");
+
+  // playButton.addEventListener("click", () => {
+  //   audioPlayer.play(); // Play the audio
+  // });
+
   return (
-    <main class="inter container">
+    <main class="container font-inconsolata">
       <div class="flex justify-around">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -30,6 +86,8 @@ export default function Home() {
         <div class="flex gap-2 justify-evenly items-center">
           <select>
             <option>Mono</option>
+            <option>Inter</option>
+            <option>Inter</option>
           </select>
           <div class="vl"></div>
           <label class="switch">
@@ -54,89 +112,33 @@ export default function Home() {
         </div>
       </div>
       <div class="flex items-center bg-white rounded-lg shadow-md">
-        <input
-          type="text"
-          class="w-full px-4 py-2 rounded-l-lg focus:outline-none"
-          placeholder="Search..."
-        />
-        <a href="#" class="flex items-center px-3 rounded-r-lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-          >
-            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-          </svg>
-        </a>
-      </div>
-      <div class="flex justify-between">
-        <div>
-          <h1 class="text-4xl font-semibold">keyboard</h1>
-          <span class="sans purple">/ki:bc:d/</span>
-        </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="75"
-          height="75"
-          viewBox="0 0 75 75"
+        <form
+          class="flex justify-between flex-shrink-0 w-full"
+          onSubmit={handleSearch}
         >
-          <g fill="#A445ED" fill-rule="evenodd">
-            <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
-            <path d="M29 27v21l21-10.5z" />
-          </g>
-        </svg>
+          <input
+            type="text"
+            class="px-4 py-2 rounded-l-lg focus:outline-none flex-grow"
+            placeholder="Search..."
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button
+            type="submit"
+            class="flex items-center px-3 rounded-r-lg"
+            aria-label="Search"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 512 512"
+            >
+              <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+            </svg>
+          </button>
+        </form>
       </div>
-      <div class="flex items-center">
-        <span class="text-lg font-bold italic mr-4">noun</span>
-        <hr class="flex-grow border-t border-gray-300" />
-      </div>
-      <div>
-        <h6 class="grey text-base">Meaning</h6>
-        <div>
-          <ul class="marker:text-green list-outside list-disc pl-3">
-            <li class="purple pl-3">
-              <div class="black">
-                (etc.) A set of keys used to operate a typewriter, computer etc.
-              </div>
-            </li>
-            <li class="purple pl-3">
-              <div class="black">
-                A component of many instruments including the piano, organ, and
-                harpsichord consisting of usually black and white keys that
-                cause different tones to be produced when struck.
-              </div>
-            </li>
-            <li class="purple pl-3">
-              <div class="black">
-                A device with keys of a musical keyboard, used to control
-                electronic sound-producing devices which may be built into or
-                separate from the keyboard device.
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <span>Synonyms</span>
-          <span class="purple">electronic keyboard</span>
-        </div>
-      </div>
-      <div>
-        <div class="flex items-center">
-          <span class="text-lg font-bold italic mr-4">verb</span>
-          <hr class="flex-grow border-t border-gray-300" />
-        </div>
-        <div>
-          <span class="grey text-base">Meaning</span>
-          <ul class="list-disc list-outside pl-3">
-            <li class="purple pl-3">
-              <div class="black">To type on a computer keyboard</div>
-            </li>
-          </ul>
-          <p class="grey text-base">
-            &quot;Keyboarding is the part of this job I hate the most.&quot;
-          </p>
-        </div>
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: elementsString }} />
       <br />
       <div>
         <p class="grey underline text-sm">Source</p>
